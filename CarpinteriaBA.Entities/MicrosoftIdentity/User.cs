086f8c340da6f1ce2a3229d12f48c5fc5dc134d0
Matias Flores <matiasflores@outlook.com>
2026-06-30 21:17:33 -0300

    ComienzoDeIdentity

---
+++ CarpinteriaBA.Entities/MicrosoftIdentity/User.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CarpinteriaBA.Entities.MicrosoftIdentity
+{
+    public class User:IdentityUser<Guid>
+    {
+
+        [Required(ErrorMessage = "{0} Required")]
+        [StringLength(100)]
+        [PersonalData]
+        public string Nombres { get; set; }
+        [Required(ErrorMessage = "{0} Required")]
+        [StringLength(100)]
+        [PersonalData]
+        public string Apellidos { get; set; }
+        [StringLength(20)]
+        public string Telefono { get; set; } = null!;
+    }
+}
